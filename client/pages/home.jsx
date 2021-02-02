661115d50451818c6b035a288455095d4b7aa2b1
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-01 19:51:39 -0800

    finished step 1 and added other needed files

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React from "react"
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+const types = ["Chest", "Back", "Biceps", "Triceps", "Shoulders", "Legs", "Abs"]
+
+export default class Home extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={}
+  }
+
+  render(){
+    return (
+      <div className="container">
+        {
+          types.map(type=>{
+            return (
+            <div className="row">
+              <div className="button-outline">
+                <button className="type-button">{type}</button>
+              </div>
+            </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
