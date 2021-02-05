c0c36e0c90a67aba7a63051f51406152d244a20b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-04 23:46:29 -0800

    minor css edits

--- client/pages/routines.jsx
+++ client/pages/routines.jsx
@@ -69,14 +69,14 @@ export default class Routines extends React.Component{
             </div>
           </div>
           <div className="card">
-            <div className="card-header" id="headingThree">
+            <div className="card-header" id="headingFour">
               <h2 className="mb-0">
-                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                   Wednesday
                 </button>
               </h2>
             </div>
-            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div id="collapseFour" className="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
               <div className="card-body">
                 {
 
@@ -85,14 +85,14 @@ export default class Routines extends React.Component{
             </div>
           </div>
           <div className="card">
-            <div className="card-header" id="headingThree">
+            <div className="card-header" id="headingFive">
               <h2 className="mb-0">
-                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                   Thursday
                 </button>
               </h2>
             </div>
-            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div id="collapseFive" className="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
               <div className="card-body">
                 {
 
@@ -101,14 +101,14 @@ export default class Routines extends React.Component{
             </div>
           </div>
           <div className="card">
-            <div className="card-header" id="headingThree">
+            <div className="card-header" id="headingSix">
               <h2 className="mb-0">
-                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                   Friday
                 </button>
               </h2>
             </div>
-            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div id="collapseSix" className="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
               <div className="card-body">
                 {
 
@@ -117,14 +117,14 @@ export default class Routines extends React.Component{
             </div>
           </div>
           <div className="card">
-            <div className="card-header" id="headingThree">
+            <div className="card-header" id="headingSeven">
               <h2 className="mb-0">
-                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <button className="accordion-button btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                   Saturday
                 </button>
               </h2>
             </div>
-            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div id="collapseSeven" className="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
               <div className="card-body">
                 {
 
