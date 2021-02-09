b68c5f2ced560eea49668242b43f344ff11197a0
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-08 20:26:26 -0800

    added stopwatch

---
+++ client/pages/stopwatch.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+
+export default class Stopwatch extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={start: false, counter: 0}
+    this.tick = this.tick.bind(this)
+    this.play = this.play.bind(this)
+    this.pause = this.pause.bind(this)
+    this.clear = this.clear.bind(this)
+  }
+
+  tick() {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  play() {
+    this.setState({ start: true })
+    this.timer = setInterval(() => this.tick(), 1000)
+  }
+
+  pause() {
+    clearInterval(this.timer)
+    this.setState({ start: false })
+  }
+
+  clear() {
+    this.setState({ counter: 0 })
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h2 className="text-center header">Stopwatch</h2>
+        {
+          this.state.start
+            ? <div className="row-watch">
+                <div className="watch">
+                  <h1>{this.state.counter}</h1>
+                </div>
+                <i onClick={this.pause} className="fas fa-pause"></i>
+              </div>
+            : <div className="row-watch">
+                <h4 className="watch-tip">Click on watch to reset</h4>
+                <div onClick={this.clear} className="watch" style={{cursor:"pointer"}}>
+                  <h1>{this.state.counter}</h1>
+                </div>
+                <i onClick={this.play} className="fas fa-play"></i>
+              </div>
+        }
+      </div>
+    )
+  }
+}
