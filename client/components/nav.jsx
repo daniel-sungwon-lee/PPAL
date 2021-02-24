27de8e2060602987a457df7857102e2109b0f11b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-23 16:33:01 -0800

    added link to logo

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -30,7 +30,7 @@ export default class Nav extends React.Component {
               <div className="username-greeting">
                 <h4>{`Hello, ${this.props.user.username}!`}</h4>
               </div>
-              <img src="icons/picon.png" className="icofont-muscle" alt="PPAL logo"/>
+              <a href="#"><img src="icons/picon.png" className="icofont-muscle" data-toggle="collapse" data-target="#navbarToggleExternalContent" alt="PPAL logo"/></a>
             </div>
           </div>
           </>
